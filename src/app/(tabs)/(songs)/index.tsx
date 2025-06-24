8b6726e3cf67840005ddb4893fff3ba17d19b4c8
ayushv <rwtsiya025@gmail.com>
2025-06-24 23:57:57 +0530

    (songs)- layout and index started

---
+++ src/app/(tabs)/(songs)/index.tsx
@@ -0,0 +1,12 @@
+import { ScrollView, View, Text } from 'react-native'
+import { defaultStyles } from '@/styles'
+
+const SongsScreen = () => {
+    return(
+        <View style={defaultStyles.container}>
+            <Text style={defaultStyles.text}>Songs Screen</Text>
+        </View>
+    )
+}
+
+export default SongsScreen
